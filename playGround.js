f949cfd92c7d5799a5d5d89ef79715e5c12ada28
shubhamkadu <shubhampkadu@gmail.com>
2026-05-07 21:15:17 +0530

    interval problem

--- playGround.js
+++ playGround.js
@@ -2,16 +2,28 @@ module.exports = {
   //param A : intiger array
   //return an integer
   solve: function (A) {
-    let ans = Number.MIN_SAFE_INTEGER
-    let sum = 0
+    let ans = []
+    a1 = A[0][0]
+    b1 = A[0][1]
     for(let i = 0;i<A.length;i++){
-      sum +=A[i]
-      ans = Math.max(ans, sum);
-      if (sum < 0) sum = 0;
+      [a2,b2] =[A[i][0],A[i][1]]
+      if(a2>b1){
+        ans.push([a1,b1])  
+        a1 = a2
+        b1 = b2
+      }else{
+        a1 = Math.min(a1,a2)
+        b1 = Math.max(b1,b2)
+      }
     }
+    ans.push([a1,b1])
     return ans
-   
   },
 };
-A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; 
+A = [
+  [1, 3],
+  [2, 6],
+  [8, 10],
+  [15, 18],
+];
 console.log(module.exports.solve(A));
