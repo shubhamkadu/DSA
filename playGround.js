6e38f952f6d074c57f8167873234507babb879d4
shubhamkadu <shubhampkadu@gmail.com>
2026-05-10 12:34:32 +0530

    permutation

--- playGround.js
+++ playGround.js
@@ -1,15 +1,63 @@
 module.exports = {
-  //param A : intiger array
-  //return an integer
-  solve: function (A) {
+  nextPermutation: function (A) {
+    let n = A.length;
 
-    console.log(A.length)
+    if (n === 1) return A;
 
-    for(let i = 0;i<A.length;i++){
-      console.log(A[i])
+    let pivotIndex = -1;
+
+    // find pivot
+    for (let i = n - 2; i >= 0; i--) {
+      if (A[i] < A[i + 1]) {
+        pivotIndex = i;
+        break;
+      }
     }
-    
+
+    // reverse whole array if no pivot
+    if (pivotIndex === -1) {
+      reverse(A, 0, n - 1);
+      return A;
+    }
+
+    let swapIndex = -1;
+
+    // find next greater element
+    for (let i = n - 1; i >= 0; i--) {
+      if (A[i] > A[pivotIndex]) {
+        swapIndex = i;
+        break;
+      }
+    }
+
+    // swap
+    doSwap(A, pivotIndex, swapIndex);
+
+    // reverse suffix
+    reverse(A, pivotIndex + 1, n - 1);
+
+    return A;
+
+    // reverse helper
+    function reverse(arr, i, j) {
+      while (i < j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        i++;
+        j--;
+      }
+    }
+
+    // swap helper
+    function doSwap(arr, i, j) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
   },
 };
-A = [1,2,3,4,5];
+A = [1, 2, 3, 4, 5];
 console.log(module.exports.solve(A));
+
