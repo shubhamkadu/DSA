c8ba6f10187c179384f18faa8e90c1ba499b122e
shubhamkadu <shubhampkadu@gmail.com>
2026-05-19 00:00:50 +0530

    hasing

--- Day-24-Lab Hashing/Subarray Sum Equals K.js
+++ Day-24-Lab Hashing/Subarray Sum Equals K.js
@@ -48,25 +48,27 @@ Explanation 1:
 All the possible subarrays having sum 0.
 ```;
 
+// YOUR CODE GOES HERE
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+let A = 13;
+if (A === 1) return 1;
+let memo = new Array(A + 1).fill(-1);
+function countMinPerfSquares(n) {
+  let INT_MAX = Number.MAX_SAFE_INTEGER;
 
-module.exports = {
-  solve: function (A, B) {
-    let map = {};
+  if (n == 0) return 0;
+  if (memo[n] !== -1) return memo[n];
+  let ans = INT_MAX;
+  let i = 1;
 
-    map[0] = 1;
+  while (i * i <= n) {
+    ans = Math.min(ans, 1 + countMinPerfSquares(n - i * i));
+    i++;
+  }
+  memo[n] = ans;
+  return memo[n];
+}
+console.log(countMinPerfSquares(A));
 
-    let sum = 0;
-    let result = 0;
-
-    for (let i = 0; i < A.length; i++) {
-      sum += A[i];
-
-      if (map[sum - B]) {
-        result += map[sum - B];
-      }
-      map[sum] = (map[sum] || 0) + 1;
-    }
-    return result;
-  },
-};
 
