b295938a1252339d47690dd6e2ce13fd81cda603
shubhamkadu <shubhampkadu@gmail.com>
2026-05-22 16:18:31 +0530

    quick sort

---
+++ DSA-26-Quick Sort And Comparator/Quick Sort.js
@@ -0,0 +1,83 @@
+```
+Problem Description
+
+Given an integer array A, sort the array using Quick Sort.
+
+
+Problem Constraints
+
+1 <= |A| <= 105
+1 <= A[i] <= 109
+
+
+Input Format
+
+First argument is an integer array A.
+
+
+Output Format
+
+Return the sorted array.
+
+
+Example Input
+
+Input 1:-
+A = [1, 4, 10, 2, 1, 5]
+Input 2:-
+A = [3, 7, 1]
+
+
+Example Output
+
+Output 1:-
+[1, 1, 2, 4, 5, 10]
+Output 2:-
+[1, 3, 7]
+
+
+Example Explanation
+
+Explanation 1 and 2:
+Return the sorted array.
+```;
+
+module.exports = {
+  //param A : array of integers
+  //return a array of integers
+  
+  solve: function (A) {
+    function partition(start, end) {
+      function swap(i, j) {
+        [A[i], A[j]] = [A[j], A[i]];
+      }
+      let pivatValue = A[start];
+      let left = start + 1;
+      let right = end;
+      while (left <= right) {
+        if (A[left] < pivatValue) {
+          left++;
+        } else if (A[right] > pivatValue) {
+          right--;
+        } else {
+          swap(left, right);
+          left++;
+          right--;
+        }
+      }
+      swap(start, right);
+      return right;
+    }
+
+    function quickSort(start, end) {
+      if (start >= end) {
+        return;
+      }
+      let pivatIndex = partition(start, end);
+      quickSort(start, pivatIndex - 1);
+      quickSort(pivatIndex + 1, end);
+    }
+    quickSort(0, A.length - 1);
+    return A;
+  },
+};
