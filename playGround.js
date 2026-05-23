4518cf201d3e321987987dc6de756880cb842a4b
shubhamkadu <shubhampkadu@gmail.com>
2026-05-23 18:40:08 +0530

    peak element

--- playGround.js
+++ playGround.js
@@ -2,27 +2,29 @@ module.exports = {
   //param A : integer
   //param B : array of integers
   //return an integer
-  solve: function (A, B) {
-    let start = 0
-    let end = A.length-1
-    console.log(end)
-    let ans = 0
-    while(start<=end){
-      let mid = start+ Math.floor((end-start)/2)
-
-      if(A[mid]>=B){
-        ans = mid
-        end = mid-1
-      }else{
-        start = mid+1
+  solve: function (A) {
+    let start = 0;
+    let n = A.length - 1;
+    let end = n;
+    while (start <= end) {
+      let mid = start + Math.floor((end - start) / 2);
+      if (
+        (mid === 0 || A[mid] > A[mid - 1]) &&
+        (mid == n || A[mid] > A[mid + 1])
+      ) {
+        return A[mid];
+      } else if (
+        (mid === 0 || A[mid] > A[mid - 1]) &&
+        (mid == n || A[mid] < A[mid + 1])
+      ) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
       }
     }
-    return ans 
   },
 };
 
+A = [5, 17, 100, 11];
 
-A = [1, 3, 5, 6];
-B = 7; 
-
-console.log(module.exports.solve(A, B));
+console.log(module.exports.solve(A));
